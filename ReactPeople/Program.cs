7b18bf481e843417cad56253711b3cdde6b12e87
Bekbab <72729623+Bekbab@users.noreply.github.com>
2022-05-16 10:59:49 +0200

    commit

--- ReactPeople/Program.cs
+++ ReactPeople/Program.cs
@@ -9,6 +9,11 @@ namespace ReactPeople
             ReactionWizard wiz = new ReactionWizard();
             bool gameIsRunning = true;
 
+            Console.WriteLine("Welcome to React People!");
+            Console.WriteLine("A super simple game where you introduce people to cats and dogs.");
+            Console.WriteLine("Press enter to start.");
+            Console.ReadLine();
+
             while (gameIsRunning)
             {
                 wiz.Update();
